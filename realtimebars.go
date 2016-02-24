77e27ed69be06eb8be8c8ec56285f0abdbfa07c7
mbalkanloo <mbalkanloo@gmail.com>
2016-02-24 08:40:16 -0500

    realtime bars serialization

--- realtimebars.go
+++ realtimebars.go
@@ -1,7 +1,15 @@
 package ib
 
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
+
 type RealTimeBarsBroker struct {
 	Broker
+	Contracts       map[int64]Contract
 	RealTimeBarChan chan RealTimeBar
 }
 
@@ -10,7 +18,7 @@ type RealTimeBarsRequest struct {
 	Bar  int64
 	Show string // what to show
 	Rth  bool   // regular trading hours
-	//	Opts string // use default "XYZ"
+	Opts string // use default "XYZ"
 }
 
 type RealTimeBar struct {
@@ -25,12 +33,80 @@ type RealTimeBar struct {
 	BarCount int64
 }
 
+func (r *RealTimeBarsBroker) BarToJSON(b *RealTimeBar) ([]byte, error) {
+	c := r.Contracts[b.Rid]
+	return json.Marshal(struct {
+		Time         string
+		Symbol       string
+		SecurityType string
+		Exchange     string
+		Currency     string
+		Right        string
+		Strike       float64
+		Expiry       string
+		BarTime      string
+		Open         float64
+		High         float64
+		Low          float64
+		Close        float64
+		Volume       int64
+		WAP          float64
+		BarCount     int64
+	}{
+		Time:         strconv.FormatInt(time.Now().UTC().Add(-5*time.Hour).UnixNano(), 10),
+		Symbol:       c.Symbol,
+		SecurityType: c.SecurityType,
+		Exchange:     c.Exchange,
+		Currency:     c.Currency,
+		Right:        c.Right,
+		Strike:       c.Strike,
+		Expiry:       c.Expiry,
+		BarTime:      b.Time,
+		Open:         b.Open,
+		High:         b.High,
+		Low:          b.Low,
+		Close:        b.Close,
+		Volume:       b.Volume,
+		WAP:          b.WAP,
+		BarCount:     b.BarCount,
+	})
+}
+
+func (r *RealTimeBarsBroker) BarToCSV(b *RealTimeBar) string {
+	c := r.Contracts[b.Rid]
+	return fmt.Sprintf(
+		"%s,%s,%s,%s,%s,%s,%.2f,%s,%s,%.2f,%.2f,%.2f,%.2f,%d,%.2f,%d",
+		strconv.FormatInt(time.Now().UTC().Add(-5*time.Hour).UnixNano(), 10),
+		c.Symbol,
+		c.SecurityType,
+		c.Exchange,
+		c.Currency,
+		c.Right,
+		c.Strike,
+		c.Expiry,
+		b.Time,
+		b.Open,
+		b.High,
+		b.Low,
+		b.Close,
+		b.Volume,
+		b.WAP,
+		b.BarCount,
+	)
+}
+
 func NewRealTimeBarsBroker() RealTimeBarsBroker {
-	r := RealTimeBarsBroker{Broker{}, make(chan RealTimeBar)}
+	r := RealTimeBarsBroker{
+		Broker{},
+		make(map[int64]Contract),
+		make(chan RealTimeBar),
+	}
+
 	return r
 }
 
 func (r *RealTimeBarsBroker) SendRequest(rid int64, d RealTimeBarsRequest) {
+	r.Contracts[rid] = d.Con
 	r.WriteInt(REQUEST.CODE.REALTIMEBARS)
 	r.WriteInt(REQUEST.VERSION.REALTIMEBARS)
 	r.WriteInt(rid)
